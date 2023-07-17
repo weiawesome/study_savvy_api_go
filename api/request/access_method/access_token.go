f0db443b37c133ef41aaf8b1773d417e86736f87
tcweeei <open891013@gmail.com>
2023-07-17 14:48:59 +0800

    Add request of access_method.

---
+++ api/request/access_method/access_token.go
@@ -0,0 +1,6 @@
+package access_method
+
+type AccessMethodAccessToken struct {
+	AesKey      string `json:"AES_key"`
+	AccessToken string `json:"access_token"`
+}
---
+++ api/request/access_method/api_key.go
@@ -0,0 +1,6 @@
+package access_method
+
+type AccessMethodApiKey struct {
+	AesKey string `json:"AES_key"`
+	ApiKey string `json:"api_key"`
+}
