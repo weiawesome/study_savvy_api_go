f6930bcc9453de72829428f7347a74f82a8e8e51
tcweeei <open891013@gmail.com>
2023-07-17 14:38:38 +0800

    Add request of user.

---
+++ api/request/user/login_app.go
@@ -0,0 +1,6 @@
+package user
+
+type LoginApp struct {
+	Mail     string `json:"mail"`
+	Password string `json:"password"`
+}
--- study_savvy_api_go.go
+++ study_savvy_api_go.go
@@ -2,22 +2,20 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
-	}
-
-	log.Printf("Listening on port %s", port)
-	log.Printf("Open http://localhost:%s in the browser", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	//http.HandleFunc("/", indexHandler)
+	//port := os.Getenv("PORT")
+	//if port == "" {
+	//	port = "8080"
+	//	log.Printf("Defaulting to port %s", port)
+	//}
+	//
+	//log.Printf("Listening on port %s", port)
+	//log.Printf("Open http://localhost:%s in the browser", port)
+	//log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
