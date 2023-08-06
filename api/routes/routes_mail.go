993c9b9bca4c67e782811fae7b33b06a721e4ca0
tcweeei <open891013@gmail.com>
2023-08-06 16:30:47 +0800

    Add mail.

--- api/routes/routes_mail.go
+++ api/routes/routes_mail.go
@@ -2,12 +2,14 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	handlerMail "study_savvy_api_go/api/handler/mail"
+	requestMail "study_savvy_api_go/api/middleware/request/mail"
 	"study_savvy_api_go/internal/repository/redis"
 	"study_savvy_api_go/internal/repository/sql"
+	"study_savvy_api_go/internal/service/mail"
 )
 
 func InitMailRoutes(r *gin.RouterGroup, sqlRepository *sql.Repository, redisRepository *redis.Repository) {
-	//middlewareJwt := jwt.MiddlewareJwt{Repository: redisRepository}
-	//r.POST("/", AuthHomeHandler)
-	//r.GET("/:mail/:code", AuthHomeHandler)
+	r.POST("", requestMail.MiddlewareMailVerificationContent(), (&handlerMail.HandlerMailVerification{Service: mail.ServiceMailVerification{RedisRepository: *redisRepository}}).Handle)
+	r.GET("/:mail/:code", requestMail.MiddleWareMailVerifyContent(), (&handlerMail.HandlerMailVerify{Service: mail.ServiceMailVerify{RedisRepository: *redisRepository}}).Handle)
 }
